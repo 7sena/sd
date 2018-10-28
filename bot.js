8ba28420ab6b5f2b573c76c2dbf14b0db868dd81
7sena <40254036+7sena@users.noreply.github.com>
2018-10-28 11:22:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ console.log(`Online`);
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var ServerID = "505995244672909323"; 
-var ChannelID = "506014273345159168";
+var ChannelID = "506016779626348555";
 
 
     console.log(`Online`);
@@ -21,4 +21,20 @@ function timerFunc() {
 
 var timer = setTimeout(timerFunc, 1);
 
-client.login("NTAzNjE1NjMyNTA0Mzg5NjMy.Drb8RQ.D0o10e9c5JPX8NLAPQwfHwszRWc");
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , â™ڑRoأ½ale#5555
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1);
+
+client.login("NDk4NDY5MjUxMzMwMzQyOTEy.DrcB3A.QPP17z4N2WRR2lU_Av_DGv_r-_s");
