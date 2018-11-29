207f8dd6d5fcb553ba028a8d68c0137d0ca4c6c7
7sena <40254036+7sena@users.noreply.github.com>
2018-11-29 21:35:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,20 +11,5 @@ client.on('ready', async() => {
     },205);
 });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "3say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
 
 client.login("NTA2MTAxOTcwODg5NjA1MTIx.DrdQvw.AkQLfbUPC7eml0MjtyZLmylzNa8");
